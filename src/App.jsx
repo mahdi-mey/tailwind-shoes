afdde003350a046b61112121478fba262d0d4991
Mahdi <mahdimey17@gmail.com>
2024-03-28 14:23:20 +0330

    init project-installed all dependancies-added assets(images)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="flex px-4 text-2xl font-bold uppercase text-green-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, sit?
+      </p>
+    </>
+  );
+}
+
+export default App;
