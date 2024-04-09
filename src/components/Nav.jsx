3873469f806f4fa168e035b2835683fb74ddcdca
Mahdi <mahdimey17@gmail.com>
2024-04-09 10:14:25 +0330

    added gradient, layout, hovers & responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,44 +1,55 @@
-import { useState } from "react";
+import { TbShoppingBag } from "react-icons/tb";
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { TbShoppingBag } from "react-icons/tb";
+import { useState } from "react";
 
-const ROUTES = ["HOME", "ABOUT", "SERVICES", "PRICING", "CONTACT"];
-
+const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 export default function Nav() {
   const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
-
   return (
-    <nav className="flex flex-wrap items-center justify-between">
-      <a href="">
+    <nav className="relative z-10 flex flex-wrap items-center justify-between">
+      {/* Logo */}
+      <a href="#">
         <NikeLogo className="h-20 w-20" />
       </a>
+
+      {/* Burger button */}
       <button
-        onClick={() => setIsMobileMenuShown((prevValue) => !prevValue)}
-        className="rounded-lg p-2 focus:ring-2 focus:ring-gray-200 md:hidden"
+        onClick={() => setIsMobileMenuShown(!isMobileMenuShown)}
+        className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 md:hidden"
       >
-        <RxHamburgerMenu size={30} />
+        <RxHamburgerMenu size={25} />
       </button>
 
+      {/* Menu list */}
       <div
-        className={`w-full md:block md:w-auto ${isMobileMenuShown === false ? " hidden" : ""}`}
+        className={`${
+          isMobileMenuShown === false && "hidden"
+        } w-full md:block md:w-auto`}
       >
-        <ul className="bg-gray-50-50 flex flex-col rounded-lg border border-gray-200 p-4 text-sm md:flex-row md:border-none md:text-base">
-          {ROUTES.map((route, index) => {
+        <ul className="flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg md:flex-row md:space-x-8 md:border-none md:bg-transparent">
+          {ROUTES.map((route, i) => {
             return (
               <li
-                className={`rounded-lg px-3 py-2 ${index === 0 ? "bg-blue-400 text-white" : "cursor-pointer hover:bg-gray-200"}`}
+                className={`cursor-pointer rounded px-3 py-2 md:hover:bg-transparent md:hover:text-blue-500 ${
+                  i === 0
+                    ? "bg-blue-500 text-white md:bg-transparent md:text-blue-500 "
+                    : "hover:bg-gray-100"
+                } ${(i == 3 || i == 4) && "md:text-white"}`}
                 key={route}
               >
-                {route}
+                <a>{route}</a>
               </li>
             );
           })}
         </ul>
       </div>
 
-      <div className="fixed bottom-4 left-4 rounded-full bg-white p-2 shadow-sm shadow-gray-500 md:static">
-        <TbShoppingBag />
+      {/* Cart button */}
+      <div className="fixed bottom-4 left-4 md:static md:mr-8">
+        <div className="flex-center h-12 w-12 cursor-pointer rounded-full bg-white shadow-md">
+          <TbShoppingBag />
+        </div>
       </div>
     </nav>
   );
--- src/components/ShoeDetail.jsx
+++ src/components/ShoeDetail.jsx
@@ -2,29 +2,33 @@ import nike1 from "../assets/n1-min.png";
 
 export default function ShoeDetail() {
   return (
-    <div className="flex-1 flex-col space-y-4 md:flex-row-reverse">
-      <div className="flex">
-        <img src={nike1} alt="" />
+    <div className="flex flex-col space-y-4 md:flex-row-reverse">
+      {/* Shoe image */}
+      <div className="flex-1 md:-mt-32 md:ml-28">
+        <div className="flex-center h-full bg-gradient-to-br from-[#F637CF] from-5% via-[#E3D876] via-40% to-[#4DD4C6]">
+          <img src={nike1} />
+        </div>
       </div>
+      <div className="flex-1 space-y-6">
+        {/* Shoe text details */}
+        <div className="text-5xl font-black md:text-9xl">Nike Air Max 270</div>
+        <div className="font-medium md:text-xl">
+          {
+            "The Nike Air Max 270 is a lifestyle shoe that's sure to turn heads with its vibrant color gradient."
+          }
+        </div>
+        <div className="text-3xl font-extrabold md:text-6xl">100 $</div>
 
-      <div>
-        <div className="flex-1 text-5xl md:text-9xl">
-          Nike Air VaporMax Plus
-        </div>
-        <div className="text-xl md:font-medium">
-          The Nike Air VaporMax Plus looks to the past and propels you into the
-          future.
-        </div>
-        <div className="text-3xl font-extrabold md:text-6xl  ">100$</div>
-        <div className="space-x-5">
-          <button className="rounded-lg bg-green-700 px-5 py-3 text-white transition-all hover:bg-green-800">
-            Add To Bag
+        {/* Shoe buttons and links */}
+        <div className="space-x-10">
+          <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
+            Add to bag
           </button>
           <a
             href="#"
             className="text-lg font-bold underline underline-offset-4"
           >
-            View Details
+            View details
           </a>
         </div>
       </div>
