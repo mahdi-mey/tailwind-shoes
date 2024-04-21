a73d83caaf1ddc835ca005bed0e3a0fd123e45e2
Mahdi <mahdimey17@gmail.com>
2024-04-21 13:13:59 +0330

    added shoe item with styles and hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Nav from "./components/Nav";
-import ShoeDetail from "./components/ShoeDetail";
+// import Nav from "./components/Nav";
+// import ShoeDetail from "./components/ShoeDetail";
+import Cart from './components/Cart'
+import { SHOE_LIST } from './constant';
 
 function App() {
   return (
     <div className="md:px24 bg-slate-100 p-4 sm:px-14 animate-fadeIn">
-      <Nav />
-      <ShoeDetail />
+      {/* <Nav />
+      <ShoeDetail /> */}
+
+      <Cart item={SHOE_LIST[0]} />
     </div>
   );
 }
