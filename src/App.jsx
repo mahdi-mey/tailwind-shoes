83650e1406e34df7d6005fd62bc347788b6d4946
Mahdi <mahdimey17@gmail.com>
2024-04-22 16:26:33 +0330

    rendered cart items responsivly with grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-// import Nav from "./components/Nav";
-// import ShoeDetail from "./components/ShoeDetail";
-import Cart from './components/Cart'
+import Nav from "./components/Nav";
+import NewArrivalsSection from "./components/NewArrivalsSection";
+import ShoeDetail from "./components/ShoeDetail";
+// import Cart from './components/Cart'
 import { SHOE_LIST } from './constant';
 
 function App() {
   return (
     <div className="md:px24 bg-slate-100 p-4 sm:px-14 animate-fadeIn">
-      {/* <Nav />
-      <ShoeDetail /> */}
-
-      <Cart item={SHOE_LIST[0]} />
+      <Nav />
+      <ShoeDetail />
+      <NewArrivalsSection items={SHOE_LIST} />
     </div>
   );
 }
---
+++ src/components/NewArrivalsSection.jsx
@@ -0,0 +1,18 @@
+import Cart from "./Cart";
+
+export default function NewArrivalsSection({ items }) {
+  return (
+    <div className="mt-20">
+      <div className='flex-center"'>
+        <div className="bg-[url('./assets/lines.png')] bg-center text-center text-2xl font-extrabold">
+          NEW ARRIVALS
+        </div>
+      </div>
+      <div className="mt-10 grid grid-cols-1 justify-center gap-x-6 gap-y-16 md:grid-cols-2 xl:grid-cols-3">
+        {items.map((item) => (
+          <Cart key={items.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
